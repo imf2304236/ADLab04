5235506f138b3d38f9064bc14fef12bfa30a70a5
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-11-21 14:18:59 +0100

    Add TextFileHandler.readStringsFromFile(String, int)

--- src/TextFileHandler.java
+++ src/TextFileHandler.java
@@ -8,12 +8,48 @@ import java.nio.file.Paths;
  */
 public class TextFileHandler {
     /**
-     * File reading method to read each line of a text file and store in an array
+     * File reading method to read 1024 lines of a text file and store in an array
      * @param sourceFile String of pathname of text file to read
      * @return Array of strings of each line of the text file
      */
-    public static String[] readfromFile(String sourceFile) {
+    public static String[] readFromFile(String sourceFile) {
         String[] a = new String[1024];
+        BufferedReader reader = null;
+
+        try {
+            Path filePath = Paths.get(sourceFile).toAbsolutePath();
+            reader = new BufferedReader(new FileReader(filePath.toString()));
+            String line;
+            line = reader.readLine();
+            int i = 0;
+
+            while(line != null) {
+                a[i] = line;
+                line = reader.readLine();
+                i++;
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e){
+                    System.err.println(e);
+                }
+            }
+        }
+        return a;
+    }
+
+    /**
+     * Read lines of a text file and store strings in an array
+     * @param sourceFile String of pathname of text file to read
+     * @param N Number of lines to be read
+     * @return Array of strings read from file
+     */
+    public static String[] readStringsFromFile(String sourceFile, int N) {
+        String[] a = new String[N];
         BufferedReader reader = null;
 
         try {
